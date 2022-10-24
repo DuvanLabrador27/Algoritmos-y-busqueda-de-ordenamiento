fb428b3eb0e2edc945384f38ee4b4bf08db0c4b1
Duvanlabrador27 <carlosduvanlh@ufps.edu.co>
2022-10-23 20:57:27 -0500

    Insertion

--- Python/Insertion.py
+++ Python/Insertion.py
@@ -1,1 +1,33 @@
-print("prueba")
+import time
+inicio = time.time()
+def insertarNumero():
+    lista=[]
+    while True:
+        n=int(input("Ingresa un numero (0 terminar): "))
+        if n==0:
+            return lista
+        else:
+            lista.append(n)
+def ordenPorInsercion(lista):
+    pos=0
+    i=0
+    aux=0
+    for _ in lista:
+        pos=i
+        aux=lista[i]
+        while pos>0 and lista[pos-1]>aux:
+            lista[pos]=lista[pos-1]
+            pos=pos-1
+        lista[pos]=aux
+        i=i+1
+    return lista
+
+def mostrarLista(lista):
+    for numero in lista:
+        print(numero)
+lista=insertarNumero()
+lista=ordenPorInsercion(lista)
+print("Lista ordenada por insercion")
+mostrarLista(lista)
+fin = time.time()
+print(fin-inicio) 
