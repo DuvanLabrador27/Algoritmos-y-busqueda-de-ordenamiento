3365b11359d03d11b157aeea7be1c427fc21194a
Juan Diego Celis <61899573+JUANCELIS@users.noreply.github.com>
2022-10-24 22:56:45 -0500

    Algoritmo Burbuja

---
+++ C++/Bubblesort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i, j, tam;
+    int temp;
+    cout << "Digite el Tamaño \n" << endl;
+    cin >> tam;
+    int arreglo[tam];
+    cout << "Digite los elementos a ordenar \n" << endl;
+    for (int i = 0; i < tam; i++)
+    {
+        cin >> arreglo[i];
+    }
+
+    for (i = 1; i < tam; i++)
+        for (j = 0; j < tam - i; j++)
+            if (arreglo[j] > arreglo[j + 1])
+            {
+                /* Intercambiamos */
+                temp = arreglo[j];
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = temp;
+            }
+    cout << "El arreglo ordenado es: \n"
+         << endl;
+
+    for (int i = 0; i < tam; i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+}
