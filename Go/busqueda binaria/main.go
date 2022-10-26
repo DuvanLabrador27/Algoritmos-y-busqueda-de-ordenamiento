6b5902264fdaa859b6f9345210e1d529b7c4aa4e
saidloop <saidfernandolgon@ufps.edu.co>
2022-10-25 21:47:28 -0500

    agregan algoritmos

--- Go/busqueda binaria/main.go
+++ Go/busqueda binaria/main.go
@@ -1,19 +1,37 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5}
-
-	for j:= 1; j < len(arr); j++ {
-		actual := arr[j]
-		i := j - 1
-
-		for i >= 0 && arr[i] > actual {
-			arr[i+1] = arr[i]
-			i = i - 1
-		}
-		arr[i+1] = actual
-	}
-	fmt.Println(arr)
+	arregloDeNumeros := []int{1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 10, 12, 15, 18, 20, 21}
+	busquedaNumerica := 44
+	
+	resultadoBusquedaNumerica := busquedaBinaria(arregloDeNumeros, busquedaNumerica)
+	fmt.Printf("[Secuencial] Buscando %d en %v... el índice devuelto es %d\n", busquedaNumerica, arregloDeNumeros, resultadoBusquedaNumerica)
 }
+
+
+func busquedaBinaria(arreglo []int, busqueda int) (indice int) {
+
+	izquierda := 0
+	derecha := len(arreglo) - 1
+	/*
+	   Recordemos que For is Go's "while"
+	   https://tour.golang.org/flowcontrol/3
+	*/
+	for izquierda <= derecha {
+		indiceDelMedio := int(math.Floor((float64(izquierda+derecha) / 2)))
+		elementoDelMedio := arreglo[indiceDelMedio]
+		if elementoDelMedio == busqueda {
+			return indiceDelMedio
+		}
+		if busqueda < elementoDelMedio {
+			derecha = indiceDelMedio - 1
+		} else {
+			izquierda = indiceDelMedio + 1
+		}
+	}
+	return -1
+}
+
