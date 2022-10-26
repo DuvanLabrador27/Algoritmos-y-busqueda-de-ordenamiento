d6a1964f321fb487d2942d7a1dc106d0cf4de9c1
saidloop <saidfernandolgon@ufps.edu.co>
2022-10-25 22:03:50 -0500

    se agrega busqeuda secuencial

---
+++ Go/busqueda secuencial/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+    numeros := []int{5, 10, 15, 20, 25}
+    fmt.Println(BusquedaSecuencial(numeros, 5)) 
+}
+
+func BusquedaSecuencial(numeros []int, valor int) int {
+	// recorrer el arreglo numeros hasta encontrar el valor
+	for k, v := range numeros {
+		// verificar si el valor iterado coincide con el que se busca
+		if v == valor {
+			// retornar el indice
+			return k
+		}
+	}
+	// retornar -1 si ningun valor coincide
+	return -1
+}
+
